3ae7fc3c2637270f3c00382f7033874b4b21718b
Kyle Hughes <kyle.hughes@outlook.com>
2014-08-03 14:11:12 +0100

    slider change event

--- gui/slider.lua
+++ gui/slider.lua
@@ -15,7 +15,21 @@ function constructor(self, container, defaults)
   obj.sClass = "slider"
 
   obj.cControl = Apollo.LoadForm(slider.xmlDoc, obj.sClass, container, obj)
+  obj.nMinValue = defaults.nMinValue or 0
+  obj.nMaxValue = defaults.nMaxValue or 100
+  obj.nTickAmount = defaults.nTickAmount or 1
+  obj.nInitialValue = defaults.nInitialValue or 50
+
+  obj.fValueMod = defaults.fValueMod or function(v) return v end
+
+  obj.cControl:FindChild('slider_control'):AddEventHandler("SliderBarChanged","slider_change",obj)
+
   return obj
+end
+
+function slider:slider_change(cHandler, cControl, nValue)
+  obj.cControl:FindChild('text_value'):SetText(tostring(self.fValueMod(nValue)))
+  Print('change')
 end
 
 setmetatable(slider,{__call=constructor})
