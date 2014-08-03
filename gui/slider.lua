4baa696a798c0dbbcfbd9d855a41f800252e1208
Kyle Hughes <kyle.hughes@outlook.com>
2014-08-03 15:21:06 +0100

    Added value change callbacks and initial value setting

--- gui/slider.lua
+++ gui/slider.lua
@@ -15,21 +15,28 @@ function constructor(self, container, defaults)
   obj.sClass = "slider"
 
   obj.cControl = Apollo.LoadForm(slider.xmlDoc, obj.sClass, container, obj)
-  obj.nMinValue = defaults.nMinValue or 0
+  obj.nMinValue = defaults.nMinValue or 1
   obj.nMaxValue = defaults.nMaxValue or 100
   obj.nTickAmount = defaults.nTickAmount or 1
-  obj.nInitialValue = defaults.nInitialValue or 50
 
   obj.fValueMod = defaults.fValueMod or function(v) return v end
 
-  obj.cControl:FindChild('slider_control'):AddEventHandler("SliderBarChanged","slider_change",obj)
-
+  obj.cControl:FindChild('text_header'):SetText(defaults.sHeader or "")
+  obj.cControl:FindChild('slider_control'):AddEventHandler("SliderBarChanged","event_slider_change",obj)
+  obj.cControl:FindChild('slider_control'):SetMinMax(obj.nMinValue,obj.nMaxValue)
+  obj.cControl:FindChild('slider_control'):SetValue(obj.nMinValue)
+  obj.fChangeCallback = defaults.fChangeCallback or function(v) end
+  obj:event_slider_change()
   return obj
 end
 
-function slider:slider_change(cHandler, cControl, nValue)
-  obj.cControl:FindChild('text_value'):SetText(tostring(self.fValueMod(nValue)))
-  Print('change')
+function slider:event_slider_change()
+  self.cControl:FindChild('text_value'):SetText(tostring(self.fValueMod(self.cControl:FindChild('slider_control'):GetValue())))
+  self.fChangeCallback(self.cControl:FindChild('slider_control'):GetValue())
+end
+
+function slider:get_value()
+  return self.cControl:FindChild('slider_control'):GetValue()
 end
 
 setmetatable(slider,{__call=constructor})
