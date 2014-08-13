d276e5b232ea1e493e7bb446ad807e970f7b5e9d
Kyle Hughes <kyle.hughes@outlook.com>
2014-08-13 18:41:40 +0100

    Fix alpha being reset to blue

--- gui/colour_picker.lua
+++ gui/colour_picker.lua
@@ -43,7 +43,7 @@ function constructor(self, container, defaults)
     fChangeCallback = function(val) obj.values.g = val obj:callback() end})
   obj.cGreenSlider = sc(obj.cControl:FindChild('green_slider'), {sHeader = 'Blue: ', nMinValue = 0, nMaxValue = 255, nInitialValue = obj.values.b,
     fChangeCallback = function(val) obj.values.b = val obj:callback() end})
-  obj.cGreenSlider = sc(obj.cControl:FindChild('alpha_slider'), {sHeader = 'Alpha: ', nMinValue = 0, nMaxValue = 255, nInitialValue = obj.values.b,
+  obj.cGreenSlider = sc(obj.cControl:FindChild('alpha_slider'), {sHeader = 'Alpha: ', nMinValue = 0, nMaxValue = 255, nInitialValue = obj.values.a,
     fChangeCallback = function(val) obj.values.a = val obj:callback() end})
 
 
